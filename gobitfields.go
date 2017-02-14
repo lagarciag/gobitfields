f630d3b044ad0467d9f191e73dbdd868558f2c31
Luis Garcia <luis.a.garcia@hpe.com>
2017-02-14 13:58:47 -0600

    chg: dev: Completed ReverseMembers, all tests passing.

--- gobitfields.go
+++ gobitfields.go
@@ -4,16 +4,18 @@ import (
 	"github.com/lagarciag/bitwisebytes"
 )
 
+type MemberMetaData struct {
+	Width  uint32
+	Offset uint32
+}
+
+
+//GetFields returns a bitfield member as specified by the provided offset and width
 func GetField(inputSlice []byte,offset int, width int) (outputSlice []byte) {
-	bytesWidth := width / 8
-	bytesWidthModulus := width % 8
-	if bytesWidthModulus != 0 {
-		bytesWidth++
-	}
+	bytesWidth := getBytesSize(width)
 	tmpField , _ := bitwisebytes.ShiftRight(inputSlice,uint(offset))
 	outputSlice = tmpField[0:bytesWidth]
 	maskSlice := bitwisebytes.MakeMask(uint(len(outputSlice)),uint(width),0)
-
 	err := bitwisebytes.And(outputSlice,maskSlice)
 	if err != nil {
 		panic(err.Error())
@@ -21,4 +23,100 @@ func GetField(inputSlice []byte,offset int, width int) (outputSlice []byte) {
 	return outputSlice
 }
 
+//PutField
+func PutField(destSlice []byte, inputSlice []byte,offset int, width int) {
+	tmpSlice := make([]byte,len(destSlice))
+
+	for i , aByte := range inputSlice {
+		tmpSlice[i] = aByte
+	}
+
+	tmpSlice2, err := bitwisebytes.ShiftLeft(tmpSlice,uint(offset))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	bitwisebytes.Or(destSlice,tmpSlice2)
+}
+
+
+func PutAllFields(inputMatrix [][]byte,metadataList []MemberMetaData) (outputSlice []byte) {
+	size := 0
+	for _ , member := range metadataList {
+		size += int(member.Width)
+	}
+	bytesSize := getBytesSize(size)
+
+	outputSlice = make([]byte,bytesSize)
+
+	for i , field := range inputMatrix {
+		PutField(outputSlice,field,int(metadataList[i].Offset),int(metadataList[i].Width))
+	}
+
+	return outputSlice
+}
+
+
+//GetAllFieldsList returns a slice of all the fields specified in the metadata list
+func GetAllFieldsList(inputSlice []byte, metadataList []MemberMetaData) (outputMatrix [][]byte) {
+	outputMatrix = make([][]byte, len(metadataList))
+	for i , metadata := range metadataList {
+		field := GetField(inputSlice,int(metadata.Offset),int(metadata.Width))
+		outputMatrix[i] = field
+	}
+	return outputMatrix
+}
+
+func getBytesSize(bitSize int) (bytesSize int) {
+	bytesSize = bitSize / 8
+	mod := bitSize % 8
+	if mod != 0 {
+		bytesSize++
+	}
+	return bytesSize
+
+}
+
+func ReverseMembers(inputBytes []byte,  metadataList []MemberMetaData) (reversed []byte) {
+
+	fieldsMatrix := GetAllFieldsList(inputBytes, metadataList)
+	reversedMetadata := ReverseMemberMetadataList(metadataList)
+	reversedMatrix := reverseMembersMatrix(fieldsMatrix)
+	reversed = PutAllFields(reversedMatrix,reversedMetadata)
+	return reversed
+}
+
+func reverseMembersMatrix(input [][]byte) (output [][]byte) {
+	length := len(input)
+	output = make([][]byte, length)
+	for i , data := range input {
+		output[(length -1) - i] = data
+	}
+	return output
+}
+
+func ReverseBytes(input []byte) (output []byte) {
+	length := len(input)
+	output = make([]byte, length)
+	for i , data := range input {
+		output[(length -1) - i] = data
+	}
+	return output
+}
+
+func ReverseMemberMetadataList(input []MemberMetaData) (output []MemberMetaData) {
+	length := len(input)
+	output = make([]MemberMetaData, length)
+	offset := 0
+	for i , _ := range output {
+		newData := MemberMetaData{}
+		oldData := input[(length -1) - i]
+		newData.Offset = uint32(offset)
+		newData.Width = oldData.Width
+		offset = offset + int(oldData.Width)
+		output[i] = newData
+
+	}
+	return output
+}
 
--- gobitfields_test.go
+++ gobitfields_test.go
@@ -23,11 +23,88 @@ func TestMain(t *testing.M) {
 
 func TestGetField(t *testing.T) {
 	inputSlice := []byte{95,0}
+
 	field1 := gobitfields.GetField(inputSlice,0,4)
 	t.Log("field 1: ",field1)
 	field2 := gobitfields.GetField(inputSlice,4,4)
 	t.Log("field 2: ",field2)
-	
+
+	if field1[0] != 15 {
+		t.Error("field must be 15")
+	}
+
+	if field2[0] != 5 {
+		t.Error("field must be 5")
+	}
+
+	recoveredSlice := make([]byte,len(inputSlice))
+
+	gobitfields.PutField(recoveredSlice,field1,0,4)
+	gobitfields.PutField(recoveredSlice,field2,4,4)
+
+	t.Log("recovered slice: ", recoveredSlice)
+
+	for i, aByte := range inputSlice {
+		if aByte != recoveredSlice[i] {
+			t.Error("mistmatch byte : ", i)
+		}
+	}
+
+
+	inputSlice = []byte{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+	field3 := gobitfields.GetField(inputSlice,33,60)
+	size := len(field3)
+	t.Log("field 3: ",field3, size)
+
+	if (field3[0] != 128) && (field3[6] != 128) {
+		t.Error("mistmatch in field values")
+	}
+}
+
+func TestGetFieldSlice(t *testing.T) {
+
+	bitfield := []byte{1,1,1,2,3,3,3,3,3,3,3,3,3,3,3,3}
+
+	member1 := gobitfields.MemberMetaData{24,0}
+	member2 := gobitfields.MemberMetaData{8,24}
+	member3 := gobitfields.MemberMetaData{96,32}
+
+	metadataList := []gobitfields.MemberMetaData{member1,member2,member3}
+
+	bitfieldMatrix := gobitfields.GetAllFieldsList(bitfield,metadataList)
+
+	t.Log("matrix ", bitfieldMatrix)
+
+	bitfieldRecovered := gobitfields.PutAllFields(bitfieldMatrix,metadataList)
+
+	t.Log("recovered Matrix: ", bitfieldRecovered)
+
+	for i , field := range bitfield {
+		if bitfieldRecovered[i] != field {
+			t.Error("mismatch")
+		}
+	}
+
+	reversed := gobitfields.ReverseMembers(bitfieldRecovered,metadataList)
+	t.Log("reversed: ", reversed)
 
 }
 
+func TestReverseFields(t *testing.T) {
+
+	inputslice := []byte{1,2,3,4,5,6}
+	outpuslice := gobitfields.ReverseBytes(inputslice)
+
+	t.Log(outpuslice)
+
+}
+
+
+func TestReverseMembers(t *testing.T) {
+
+	inputslice := []byte{1,2,3,4,5,6}
+	outpuslice := gobitfields.ReverseBytes(inputslice)
+
+	t.Log(outpuslice)
+
+}
